8876c6a019df2fde5ab76ed683aea0354e52fdf5
iakdeniz64 <0897264@hr.nl>
2016-11-07 10:52:19 +0100

    trimmed text

--- views/products/products_view.js
+++ views/products/products_view.js
@@ -18,8 +18,8 @@ var ProductsViewHelper = function()
                 
                 // Add product info
                 $(product).find(".product__title").html(value.title);
-                $(product).find(".product__subtitle").append(value.genre +(" | ")+ value.pegi_age +(" | ")+ value.platform);
-                $(product).find(".product__description").html(value.description);
+                $(product).find(".product__subtitle").append(value.genre +(" | ")+ value.pegi_age +(" | ")+ value.platform); // if ps4 dit dat
+                $(product).find(".product__description").html(CutString(value.description));
                 $(product).find(".product__price").append(("€ ")+value.price);
                 $(product).find("#buttons__info").attr("href", "product/" + value.ean_number)
                 $(product).find(".product__image img").attr("src", value.image);
@@ -30,6 +30,30 @@ var ProductsViewHelper = function()
             // Remove the first list item, because this item is always empty
             $("#product__list__item").first().remove();
         });
+    }
+
+    //Show more/less
+    function CutString(text){
+        var ShowAmount = 50;
+        var WordsArray = text.split(" ");
+        if(WordsArray.length>ShowAmount){
+            var ShortString = "";
+            for(i=0; i<ShowAmount; i++){
+                ShortString += WordsArray[i] + " ";
+            }
+            return ShortString+"...";
+        }else{
+            return text;
+        }
+    };
+
+    //Platform Logo's
+    function PlatformLogo(platform){
+        if (value.platform.toUpperCase() == "PS4"){
+            var myImage = new Image(24, 24);
+            myImage.src = 'https://maxcdn.icons8.com/Android_L/PNG/24/Logos/play_station_copyrighted-24.png';
+            return Image(myImage);
+        }
     }
 
     // Execute the event when 'something' happens (when a button is clicked for example)
