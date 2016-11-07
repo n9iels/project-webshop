c83d1830583602d68da2e0f7ef98fd50e033f4f8
DOverbeeke <donaldoverbeeke@hotmail.com>
2016-11-07 11:30:23 +0100

    single product added

--- views/single_product/single_product_view.js
+++ views/single_product/single_product_view.js
@@ -1,19 +1,26 @@
-var ProductsViewHelper = function()
+var ProductViewHelper = function()
 {
     // Change the view of the page
     function setView(viewData)
     {
         $("#component").html('<div class="col-12"><div class="col-3" id="filters"></div><div class="col-9" id="products"></div></div>');
-        // load filters
-        $("#filters").load('views/products/filters.html');
 
-        // load products
-        loadProducts(viewData);
+        loadProduct(viewData);
     }
 
-    function loadProducts(viewData)
+    // Execute the event when 'something' happens (when a button is clicked for example)
+    function setActionListener(action)
     {
-        $("#products").load('views/products/products.html', function () { // waar id = 'component' doe .load......
+        // user fills in login form
+        // user presses 'login'
+
+        // if user presses 'login', perform action (make controller send email and password to API)
+        $("#component").on("submit", "#loginform", action);
+    }
+
+    function loadProduct(viewData)
+    {
+        $("#product").load('views/single_product/single_product.html', function () { // waar id = 'component' doe .load......
             $.each(viewData, function(key, value) {
                 // Clone product
                 var product = $('<div>').append($('#product__list__item').clone());
@@ -46,27 +53,10 @@ var ProductsViewHelper = function()
         });
     }
 
-    // Execute the event when 'something' happens (when a button is clicked for example)
-    function setActionListener(action)
-    {
-        $(document).ready(action); // if 'document ready' perform "action"
-    }
-
-    function setFilterListener(action)
-    {
-         // Hier het formulier versturen als we op een checkbox drukken
-        $("#component").on("click", "input[type=checkbox]", function(event) {
-            $("#product_filter").submit();
-        });
-
-        $("#component").on("submit", "#product_filter", action);
-    }
 
     // Return the methods that can be used by other programs (the controller in this case)
     return {
         setView: setView,
-        loadProducts: loadProducts,
-        setActionListener: setActionListener,
-        setFilterListener: setFilterListener
+        setActionListener: setActionListener
     }
 };
