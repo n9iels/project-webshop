9a771cf7564959a35f370950f1217a8c3941def0
Niels van der Veer (n9iels) <nielsvanderveer@outlook.com>
2016-11-09 10:41:58 +0100

    Make cart scoll

--- libraries/cart.js
+++ libraries/cart.js
@@ -4,7 +4,8 @@ $(document).ready(function()
         var cartitem = $(this).parent();
 
         $(cartitem).addClass("cart__item--removed");
-        $(cartitem).on("transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd", function() {
+        $(cartitem).on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+            console.log("remove")
             $(this).remove();
         })
     });
