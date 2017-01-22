9f3936d06baf3626bddbc6b9deb593542fb6e273
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2017-01-22 17:11:09 +0100

    #10 Save date of user

--- views/adminuser/adminuser_view.js
+++ views/adminuser/adminuser_view.js
@@ -5,26 +5,51 @@ var AdminUserViewHelper = function ()
         $(document).ready(action); // if 'document ready' perform "action"
     }
 
-    function setView(data)
+    function setView(value)
     {
         //preventDefault();
         $("#component").load('/views/adminuser/adminuser.html', function ()
         {
-            $.each(data, function(key, value)
+            $("#delete_btn").attr("data-id", value.user_id)   
+            $("#first_name").val(value.first_name);
+            $("#insertion").val(value.insertion);
+            $("#surname").val(value.surname);
+            $("#user_type").val(value.user_type);
+            $("#email").val(value.email);
+            $("#password").attr('placeholder', 'Vul hier iets in om het wachtwoord te veranderen');
+            $("#gender").val(value.gender);
+            $("#date_of_birth").val(value.date_of_birth);
+            $("#phone_number").val(value.phone_number);
+            $("#is_active").val(value.is_active);
+            $("#user_id").val(value.user_id);
+
+            var today       = new Date();
+            var dateOfBirth = new Date(value.date_of_birth);
+
+            for(i = 1; i < 32; i++)
             {
-                $("#delete_btn").attr("data-id", value.user_id)   
-                $("#first_name").val(value.first_name);
-                $("#insertion").val(value.insertion);
-                $("#surname").val(value.surname);
-                $("#user_type").val(value.user_type);
-                $("#email").val(value.email);
-                $("#password").attr('placeholder', 'Vul hier iets in om het wachtwoord te veranderen');
-                $("#gender").val(value.gender);
-                $("#date_of_birth").val(value.date_of_birth);
-                $("#phone_number").val(value.phone_number);
-                $("#is_active").val(value.is_active);
-                $("#user_id").val(value.user_id);
-            })
+                if (i == dateOfBirth.getDate())
+                {
+                    $("#dag").append("<option value='" + i + "' selected='selected'>" + i + "</option>");
+                } else
+                {
+                    $("#dag").append("<option value='" + i + "'>" + i + "</option>");
+                }
+            }
+
+            for (i = 1900; i < today.getFullYear() + 1; i++)
+            {
+                if (i == dateOfBirth.getFullYear())
+                {
+                    $("#jaar").append("<option value='" + i + "' selected='selected'>" + i + "</option>");
+                }
+                else
+                {
+                    $("#jaar").append("<option value='" + i + "'>" + i + "</option>");
+                }
+            }
+            
+            $("#maand").val(dateOfBirth.getMonth());
         })
     }
 
