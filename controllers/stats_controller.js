777c885ac3b18f6eb6675f21f5cae7a002035d60
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-23 19:40:25 +0100

    titels vanuit database op site laten zien op x-as

--- controllers/stats_controller.js
+++ controllers/stats_controller.js
@@ -6,50 +6,50 @@ var StatsController = function(viewHelper, model)
     // Main function, also the start startpoint for a page
     function main()
     {
-        $("#component").load('/views/graphs/graphs.html', function () { // waar id = 'component' doe .load......
-            var htmlel = $("#graph__toptenitems__cnvs");
+        // $("#component").load('/views/graphs/graphs.html', function () { // waar id = 'component' doe .load......
+        //     var htmlel = $("#graph__toptenitems__cnvs");
 
-            var gameOne = "gameOne";
+        //     var gameOne = "gameOne";
 
-            var TopTenChart = new Chart(htmlel, {
-                type: 'bar',
-                data: {
-                    labels: [gameOne, "Blue", "Yellow", "Green", "Purple", "Orange"],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-            });
-        })
+        //     var TopTenChart = new Chart(htmlel, {
+        //         type: 'bar',
+        //         data: {
+        //             labels: [gameOne, "Blue", "Yellow", "Green", "Purple", "Orange"],
+        //             datasets: [{
+        //                 label: '# of Votes',
+        //                 data: [12, 19, 3, 5, 2, 3],
+        //                 backgroundColor: [
+        //                     'rgba(255, 99, 132, 0.2)',
+        //                     'rgba(54, 162, 235, 0.2)',
+        //                     'rgba(255, 206, 86, 0.2)',
+        //                     'rgba(75, 192, 192, 0.2)',
+        //                     'rgba(153, 102, 255, 0.2)',
+        //                     'rgba(255, 159, 64, 0.2)'
+        //                 ],
+        //                 borderColor: [
+        //                     'rgba(255,99,132,1)',
+        //                     'rgba(54, 162, 235, 1)',
+        //                     'rgba(255, 206, 86, 1)',
+        //                     'rgba(75, 192, 192, 1)',
+        //                     'rgba(153, 102, 255, 1)',
+        //                     'rgba(255, 159, 64, 1)'
+        //                 ],
+        //                 borderWidth: 1
+        //             }]
+        //         },
+        //         options: {
+        //             scales: {
+        //                 yAxes: [{
+        //                     ticks: {
+        //                         beginAtZero:true
+        //                     }
+        //                 }]
+        //             }
+        //         }
+        //     });
+        // })
 
-        //ViewHelper.setActionListener(getGraph);
+        ViewHelper.setActionListener(getGraphs);
     }
 
     function getGraphs()
