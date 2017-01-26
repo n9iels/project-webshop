0fcf59759b7ad65cba4567b59d46b5dbf10ff895
iakdeniz64 <0897264@hr.nl>
2017-01-26 09:17:00 +0100

    #11.. merge.. solve..

---
+++ libraries/order.js
@@ -0,0 +1,50 @@
+$(document).on("click", "a.order__navigate", function (e) {
+    e.preventDefault();
+
+    if ($(this).hasClass("disabled") == false)
+    {
+        // Get the order step
+        var step =  $(this).data("step");
+
+        // Set the new order step as active
+        $("ul.order-steps li").removeClass("active");
+        $("ul.order-steps li." + step).addClass("active");
+
+        $(".order__step").removeClass("order__step--active");
+        $("#" + step).addClass("order__step--active");
+    }
+})
+
+$(document).on("change", "select#shipping_method", function (e)
+{
+    var val = $(this).val();
+
+    if (val != "")
+    {
+        $(this).parents(".order__step").find(".btn--next").removeClass("disabled");
+    }
+    else
+    {
+        $(this).parents(".order__step").find(".btn--next").addClass("disabled");
+    }
+
+    $(".shipping__fields").hide();
+    $("." + val + "_fields").show();
+})
+
+$(document).on("change", "select#payment_method", function (e)
+{
+    var val = $(this).val();
+
+    if (val != "")
+    {
+        $(this).parents(".order__step").find(".btn--next").removeClass("disabled");
+        $(this).parents(".order__step").find(".btn--next").attr("disabled", false);
+    }
+    else
+    {
+        $(this).parents(".order__step").find(".btn--next").addClass("disabled");
+        $(this).parents(".order__step").find(".btn--next").attr("disabled", true);
+        
+    }
+});
