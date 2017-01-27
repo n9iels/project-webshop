bf4f287e92aa927a9d635a44d6cd065a1dcda354
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-28 00:44:19 +0100

    #22 favorite list af

--- libraries/helpers/favoritelist.js
+++ libraries/helpers/favoritelist.js
@@ -10,10 +10,11 @@ function FavoriteListHelper()
      * 
      * @return {string}
      */
-    this.addItem = function(ean_number)
+    //this.addItem = function(ean_number)
+    var addItem = function(ean_number)
     {
         $.ajax({
-            url: "https://localhost:8081/favoritelist/" + CookieHelper.getCookie("user_id") + "/" + ean_number, // "https://api.az-games.nl/favoritelist/"
+            url: "http://localhost:8081/favoritelist/" + ean_number, // "https://api.az-games.nl/favoritelist/"
             type: "post",
             dataType: 'json',
             data: JSON.stringify({}),
@@ -22,13 +23,17 @@ function FavoriteListHelper()
             },
             success: function (data) {
                 $(this).toggleClass("like--active");
-                alert("Product (van (im)order uit (im)order history) succesvol toegevoegd aan uw favorieten lijst."); // im staat voor imaginary/denkbeeldig
+                alert("Product succesvol toegevoegd aan je favorieten."); // im staat voor imaginary/denkbeeldig
                 // Comment to self = (ORDER + ORDER HISTORY verhaal Niels) + (FAVORIETEN LIJST verhaal Habbo) SAMENVOEGEN STRAKS OR ELSE...!!!!
             },
             error: function(xhr, status, error) {
-                alert("Product (van (im)order uit (im)order history) is al in favorieten lijst.");
+                alert("Product is al in je favorieten.");
                 // Comment to self = (ORDER + ORDER HISTORY verhaal Niels) + (FAVORIETEN LIJST verhaal Habbo) SAMENVOEGEN STRAKS OR ELSE...!!!!
             }
         });
+    }
+
+    return {
+        addItem: addItem
     }
 }
