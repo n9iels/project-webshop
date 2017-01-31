4339851de6b5bceef5b7f76e407b8534a2bac2f1
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-31 14:58:26 +0100

    #30 topgames chart working

--- views/graphs/graphs_view.js
+++ views/graphs/graphs_view.js
@@ -1,6 +1,6 @@
 var StatsViewHelper = function ()
 {
-    function setActionListener(action)
+    function setActionListener(action1, action2, action3)
     {
         // $(document).ready(function()
         // {   
@@ -16,7 +16,7 @@ var StatsViewHelper = function ()
         //     action("quarter");
         // }); // if 'document ready' perform "action"
 
-        $(document).ready(function(action1, action2, action3)
+        $(document).ready(function()
         {
             // load page
             // put range switch to default
@@ -45,7 +45,7 @@ var StatsViewHelper = function ()
                     $("#range_ed").append("<option value='" + i + "'>" + i + "</option>");
                 }
 
-                for (i = 1900; i < cur_date.getFullYear() + 1; i++)
+                for (i = 2015; i < cur_date.getFullYear() + 1; i++)
                 {
                     if (i == 2000)
                     {
@@ -121,14 +121,9 @@ var StatsViewHelper = function ()
 
     function setRangeChangeListeners(showTopGames, showRevenue, showUserAmnt)
     {
-        var ids = ["#range_by", "#range_bm", "#range_bd", "#range_ey", "#range_em", "#range_ed"];
-
-        $.each(ids, function(id)
+        $("#component").on("change", ".range", function()
         {
-            $("#component").on("change", id, function()
-            {
-                showGraphByHtml(showTopGames, showRevenue, showUserAmnt);
-            })
+            showGraphByHtml(showTopGames, showRevenue, showUserAmnt);
         })
     }
 
@@ -151,10 +146,79 @@ var StatsViewHelper = function ()
     }
 
     // functions for showing a graph
-    function showTopGamesGraph(data)
+    function showTopGamesGraph(graphData)
     {
-        console.log("in showTopGamesGraph:");
+        // graph
+        var labels = new Array();
+        var data = [];
+
+        var gameCount = 0;
+        var last_rank_num = 0; //assuming value.rank is never 0!
+        var highestVal = 0;
+
+        $.each(graphData, function(key, value) {
+            gameCount++;
+
+            if (gameCount < 11 || value.rank == last_rank_num) {
+                labels.push(value.title);
+                data.push(value.user_count);
+                
+                if (gameCount == 10) {
+                    last_rank_num = value.rank;
+                }
+            }
+        });
+        
+        console.log(labels);
         console.log(data);
+
+        var el = $("#graph__cnvs");
+        //remove old chart
+        el.empty();
+        //put new chart in
+        var TopGamesChart = new Chart(el, {
+            type: 'horizontalBar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: data,
+                    backgroundColor: 'rgba(0, 200, 26, 1)', //groen
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display:false
+                },
+                title: {
+                    display:true,
+                    text: "Meest verkochte games" // need in januari, in februari, etc.
+                },
+                responsive:true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            // maxRotation: 90,
+                            // minRotation: 90,
+                        }
+                    }]
+                }
+            }
+        })
+
+        // remove old description
+        var theDiv = document.getElementById("graph__description");
+        theDiv.innerHTML = "";
+        // put desciption below graph
+        var descr = 'Deze grafiek laat de games zien met het meeste aantal gebruikers dat de game 1 of meer keer heeft gekocht in de gekozen tijdsperiode.';
+        var content = document.createTextNode(descr); 
+        theDiv.appendChild(content); 
     }
     function showRevenueGraph(data)
     {
