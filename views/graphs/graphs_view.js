cb35f50b0e9f6e66d682daf3292ddc19b7d0678b
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-31 15:28:31 +0100

    #30 klein

--- views/graphs/graphs_view.js
+++ views/graphs/graphs_view.js
@@ -222,8 +222,87 @@ var StatsViewHelper = function ()
     }
     function showRevenueGraph(data)
     {
+        console.log(data);
+        $.each(data, function(key, value)
+        {
+            console.log(value.day_price);
+            console.log(value.order_date);
+            var day = value.order_date;
+            console.log
+        })
 
-    }
+
+        // graph
+        var labels = new Array();
+        var data = [];
+
+        var gameCount = 0;
+        var last_rank_num = 0; //assuming value.rank is never 0!
+        var highestVal = 0;
+
+        $.each(graphData, function(key, value) {
+            gameCount++;
+
+            if (gameCount < 11 || value.rank == last_rank_num) {
+                labels.push(value.title);
+                data.push(value.user_count);
+                
+                if (gameCount == 10) {
+                    last_rank_num = value.rank;
+                }
+            }
+        });
+        
+        console.log(labels);
+        console.log(data);
+
+        var el = $("#graph__cnvs");
+        //remove old chart
+        el.empty();
+        //put new chart in
+        var TopGamesChart = new Chart(el, {
+            type: 'horizontalBar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: data,
+                    backgroundColor: 'rgba(0, 200, 26, 1)', //groen
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display:false
+                },
+                title: {
+                    display:true,
+                    text: "Meest verkochte games" // need in januari, in februari, etc.
+                },
+                responsive:true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            // maxRotation: 90,
+                            // minRotation: 90,
+                        }
+                    }]
+                }
+            }
+        })
+
+        // remove old description
+        var theDiv = document.getElementById("graph__description");
+        theDiv.innerHTML = "";
+        // put desciption below graph
+        var descr = 'Deze grafiek laat de games zien met het meeste aantal gebruikers dat de game 1 of meer keer heeft gekocht in de gekozen tijdsperiode.';
+        var content = document.createTextNode(descr); 
+        theDiv.appendChild(content);     }
     function showUserAmntGraph(data)
     {
 
